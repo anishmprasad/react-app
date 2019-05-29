02e52ef820b0cab7bcd80e10e04ef062cc3c7544
anish.m.prasad <anish.m.prasad@gmail.com>
2019-05-29 23:47:50 +0530

    redux update

---
+++ src/containers/Root/index.js
@@ -0,0 +1,55 @@
+/*eslint-disable */
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+// import 'styles/index.scss';
+import App from '../../App';
+
+class Root extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		{
+			<Provider store={this.props.store}>
+				{
+					<BrowserRouter>
+						<Switch>
+							<Route path='/' component={App} />
+						</Switch>
+					</BrowserRouter>
+				}
+			</Provider>;
+		}
+	}
+}
+
+Root.propTypes = {
+	store: PropTypes.object
+};
+
+// function mapStateToProps(state) {
+// 	return {
+// 		goal_and_exams: state.atgData.goal_and_exams,
+// 		userinfo: state.atgData.userInfo
+// 	};
+// }
+// function mapDispatchToProps(dispatch) {
+// 	return bindActionCreators(
+// 		{
+// 			verifyUser,
+// 			registerCount,
+// 			user_details,
+// 			fetch_goal_and_exams,
+// 			get_ip_details
+// 		},
+// 		dispatch
+// 	);
+// }
+
+export default connect(
+	null,
+	null
+)(Root);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/configureStore';
+// import 'babel-polyfill';
+import Root from './containers/common/Root';
+const store = configureStore();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
