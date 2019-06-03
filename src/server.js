2dc1a4b34ce8b27fcbce863e7ddc63b4738b3166
anish.m.prasad <anish.m.prasad@gmail.com>
2019-06-03 22:18:00 +0530

    update

--- src/server.js
+++ src/server.js
@@ -6,6 +6,20 @@ import schema from './schema';
 
 const app = express();
 
+// mongoose.connect('mongodb://localhost:27017/graphqlapi');
+mongoose.connect('http://smart-meeting.herokuapp.com');
+
+const db = mongoose.connection;
+db.on('error', () => {
+	console.log('Failed to connect to database');
+}).once('open', () => {
+	console.log('Connected to DB');
+});
+
+app.get('/', (req, res) => {
+	res.send('Welcome to GraphQL Server');
+});
+
 app.use(
 	'/graphql',
 	graphqlHTTP(() => ({
@@ -14,3 +28,6 @@ app.use(
 		pretty: true
 	}))
 );
+
+// Server: http://smart-meeting.herokuapp.com
+// PlayGround - http://smart-meeting.herokuapp.com/graphql
